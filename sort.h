c65a1be54597943fa118103e808b08fd2aa08583
amessoudi <amessoudi150@gmail.com>
2024-05-02 14:01:27 +0100

    doing this the right way

--- sort.h
+++ sort.h
@@ -1,16 +1,31 @@
-#ifndef SORT_H_
-#define SORT_H_
+#ifndef SORT_H
+#define SORT_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// Prototypes for provided functions
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
 
-// Prototype for your sorting function (assuming it's named bubble_sort)
-void bubble_sort(int *array, size_t size);
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
-// Structure definition for doubly linked list (provided)
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
 	const int n;
@@ -18,4 +33,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-#endif /* SORT_H_ */
+/* Printing helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sorting algoritms */
+void bubble_sort(int *array, size_t size);
+#endif /* SORT_H */
