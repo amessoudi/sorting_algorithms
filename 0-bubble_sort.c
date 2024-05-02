b1840533e268c3ce68e66e97c24bac83b77e5e0a
amessoudi <amessoudi150@gmail.com>
2024-05-02 13:58:35 +0100

    Fixed helper functions

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,9 +15,11 @@
 
 void swap(int *a, int *b)
 {
-	int temp = *a;
+	int tmp;
+
+	tmp = *a;
 	*a = *b;
-	*b = temp;
+	*b = tmp;
 }
 /**
  *bubble_sort - Sorts an array in ascending order.
@@ -28,24 +30,24 @@ void swap(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-	int swapped = 0;
+	size_t i, len = size;
+	bool bubbly = false;
 
-	for (size_t i = 0; i < size - 1; i++)
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
 	{
-		swapped = 0;
-
-		for (size_t j = 0; j < size - i - 1; j++)
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				swap(&array[j], &array[j + 1]);
-				swapped = 1;
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
 			}
 		}
-
-		if (!swapped)
-		{
-			break;
-		}
+		len--;
 	}
 }
